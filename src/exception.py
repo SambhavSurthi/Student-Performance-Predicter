3df9d966badfbab1fa85e2b97ef736516b90eac2
SambhavSurthi <sambhavsurthi14@gmail.com>
2026-02-08 22:04:51 +0530

    Completed Logging and exception setup

---
+++ src/exception.py
@@ -0,0 +1,39 @@
+import sys
+import logging
+
+
+def error_message_details(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = (
+        "Error occurred in Python script name [{0}] "
+        "Line Number [{1}] "
+        "Error message [{2}]"
+    ).format(
+        file_name,
+        exc_tb.tb_lineno,
+        str(error)
+    )
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(
+            error=error_message,
+            error_detail=error_details
+        )
+
+    def __str__(self):
+        return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        1 / 0
+    except Exception as e:
+        logging.info("Divide By Zero error occurred")
+        raise CustomException(e, sys)
